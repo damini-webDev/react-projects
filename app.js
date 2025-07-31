b2787897a5f4ed4dae710ff2e664e8d09c724654
Damini Patil <daminipatil@Daminis-MacBook-Air.local>
2025-07-31 22:41:20 +0530

    first-project

---
+++ app.js
@@ -0,0 +1,10 @@
+const div = React.createElement("div" , {id : "parent"},
+    [React.createElement("div" , {id : "child"} ,
+    [React.createElement("h1",{} , "i am h1 tag"),React.createElement("h2",{} , "i am h2 tag")]),
+React.createElement("div" , {id : "child2"} ,
+    [React.createElement("h1",{} , "i am h3 tag"),React.createElement("h2",{} , "i am h4y tag")])])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));     
+root.render(div)
+
+
